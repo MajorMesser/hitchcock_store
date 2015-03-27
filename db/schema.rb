85dff4e81f93fb3f5c1ef57cb6e2ddf6f4af335e
Jordan P. Day <jday27@mymts.net>
2015-03-27 11:24:21 -0500

    got the mgiration working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150327160913) do
+ActiveRecord::Schema.define(version: 20150327162257) do
 
   create_table "abouts", force: true do |t|
     t.string   "title"
@@ -104,6 +104,7 @@ ActiveRecord::Schema.define(version: 20150327160913) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "category_id"
+    t.string   "image"
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id"
