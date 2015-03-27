be476b660ecd12592d41d9693bdb4570204af973
Jordan P. Day <jday27@mymts.net>
2015-03-27 11:18:53 -0500

    trying to re-create the image column in Products. Failing.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150324184856) do
+ActiveRecord::Schema.define(version: 20150327160913) do
 
   create_table "abouts", force: true do |t|
     t.string   "title"
